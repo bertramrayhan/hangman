9f25f1bbb80fef398a3ebf3fb880d7d8ac852a6e
bertramrayhan <107301737+bertramrayhan@users.noreply.github.com>
2024-05-30 03:22:14 +0000

    selesai

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def draw_hangman(attempt):
   
 
 def main():
-  choosen_word = get_random_word()
-  enumerate_choosen_word = enumerate(choosen_word)
+  choosen_word = get_random_word() 
   print('Welcome to hangman!')
   print(f'The word has {len(choosen_word)} letters.')
   print('You have 7 tries to guess the word.')
@@ -22,7 +21,8 @@ def main():
 
   while attempts > 0 or '_' in display_word:
     print(draw_hangman(attempts))
-    print(''.join(display_word))
+    print(f"     {''.join(display_word)}")
+    print()
     
     guess = input('Guess a letter or the word: ').lower()
 
@@ -30,10 +30,6 @@ def main():
     #yang salah
     if not guess.isalpha() or not guess:
       print('That is not a letter')
-      continue
-    elif len(guess) == len(choosen_word) and guess != choosen_word:
-      attempts -= 1
-      print(f"Wrong guess! You have {attempts} attempts left.")
       continue
     elif guess in guessed_letter:
       print("You already guessed that letter.")
@@ -43,7 +39,7 @@ def main():
 
     #game mechanic
     if guess in choosen_word:
-      for pos, letter in enumerate_choosen_word:
+      for pos, letter in enumerate(choosen_word):
         if letter == guess:
           display_word[pos] = letter
     else:
@@ -53,10 +49,11 @@ def main():
     if guess == choosen_word:
       print(f"Congratulations! You guessed the word: {choosen_word}")
       break
-    elif '_' not in choosen_word:
+    elif '_' not in display_word:
       print(f"Congratulations! You guessed the word: {choosen_word}")
       break
-    else:
+
+    if attempts == 0:
       print(draw_hangman(attempts))
       print(f"Game over! The word was: {choosen_word}")
       break
@@ -65,4 +62,3 @@ def main():
 if __name__ == "__main__":
   main()
 
-#MASIH SALAH. PERBAIKI
